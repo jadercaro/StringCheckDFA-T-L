5f3ca8144924996ecfd757f8e6bf13e794034f90
jadercaro <96452959+jadercaro@users.noreply.github.com>
2023-02-15 17:35:22 -0500

    Comment AFD and explaining

--- AFD.py
+++ AFD.py
@@ -5,7 +5,7 @@ class AFD:
         self._unoImpar = False
 
 
-    def determinarAceptacion(self):
+    def determinarAceptacion(self): # el criterio de aceptacion: ceros pares o unos impares no importa si son o no consecutivos  
         for i in self._cadena:
             self.funcionTransicion(int(i))
 
@@ -17,7 +17,7 @@ class AFD:
         # Iteramos la cadena usando la funcion de transicion.
 
 
-    def funcionTransicion(self, simbolo):
+    def funcionTransicion(self, simbolo): # funcion de transicion: el estado actual se interpreta de leer las variables cero par y uno impar.
         if(simbolo == 0):
             if(self._ceroPar==True):
                 self._ceroPar = False
@@ -32,7 +32,7 @@ class AFD:
 
 
 
-# cadena=input(print('Ingrese la cadena de caracteres: '))
-automataPrueba = AFD("110010100")
+cadena=input(print('Ingrese la cadena de caracteres: '))
+automataPrueba = AFD(cadena)
 
 print(automataPrueba.determinarAceptacion())
